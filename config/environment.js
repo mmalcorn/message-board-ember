b570d05308a113c5fa6150ce11b285b8d136cb73
Meredith Alcorn <alcornmeredith@gmail.com>
2016-10-28 18:16:53 -0700

    create a component to display each question and author called question-display

--- config/environment.js
+++ config/environment.js
@@ -6,15 +6,24 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
       },
+
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
+    },
+
+    firebase: {
+      apiKey: "AIzaSyCqDy0uTmfvBs1i7H2vjX5Vy1jVT76HoRM",
+      authDomain: "messageboard-questions.firebaseapp.com",
+      databaseURL: "https://messageboard-questions.firebaseio.com",
+      storageBucket: "messageboard-questions.appspot.com",
     },
 
     APP: {

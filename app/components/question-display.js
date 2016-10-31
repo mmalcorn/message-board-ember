695c01a301478ed2f37ba1d00a8b03c295ab32b0
mmalcorn <alcornmeredith@gmail.com>
2016-10-31 08:13:22 -0400

    add answer model, and build relationship between answer and question

--- app/components/question-display.js
+++ app/components/question-display.js
@@ -8,4 +8,3 @@ actions: {
     },
   }
 });
-//Don't think I need this route anymore--update is being referenced in question-route now; not question-display.  We'll see
--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   question: DS.attr(),
   questionAuthor: DS.attr(),
-  additionalInfo: DS.attr()
+  additionalInfo: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });

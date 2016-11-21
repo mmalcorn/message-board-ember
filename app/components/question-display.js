bf9af7af67f55aa15b43e45d5edd62402085e766
Meredith Alcorn <alcornmeredith@gmail.com>
2016-11-21 12:53:32 -0800

    add computed property to display the number of answers per question

--- app/components/question-display.js
+++ app/components/question-display.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
    favoriteQuestion: Ember.inject.service(),
 
+   answerToQuestion: Ember.computed(function() {
+    var answers_per_question = this.get('question.answers.length');
+    return answers_per_question;
+}),
+
+
 actions: {
   update(question, params) {
     this.sendAction('update', question, params);

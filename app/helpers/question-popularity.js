49cb1bcea70b93d32f19c1f4799ca8aa143081e3
Meredith Alcorn <meredithalcorn@Merediths-MacBook-Pro.local>
2016-12-21 16:00:34 -0800

    add helper to display a symbol next to unanswered questions.  work in progress

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,11 @@
+// import Ember from 'ember';
+//
+// export function questionPopularity(params) {
+//   var question = params[0];
+//   if(question.get('question').get('length') < 1) {
+//       return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+//   }
+// }
+//
+//
+// export default Ember.Helper.helper(questionPopularity);
